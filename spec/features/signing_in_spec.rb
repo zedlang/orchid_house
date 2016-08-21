a6345251da1bc361b55785e6e852552378baa8a8
Zareh Langridge <zslangridge@mac.com>
2016-08-21 17:35:46 +0100

    Added admin field (type boolean) to users table

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -12,3 +12,17 @@ RSpec.feature "Users can sign in" do
     expect(page).to have_content "Signed in as #{user.email}"
   end
 end
+
+RSpec.feature "Admin users can sign in" do
+  let!(:user) { FactoryGirl.create(:user, :admin) }
+  scenario "with valid credentials" do
+    visit "/"
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_button "Log in"
+    expect(page).to have_content "Signed in successfully."
+    expect(page).to have_content "Signed in as #{user.email}"
+  end
+end
+
