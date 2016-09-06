88f45042d2e81a6fee160730f8bfdfb682d253e3
Zareh Langridge <zslangridge@mac.com>
2016-09-06 14:46:15 +0100

    Addresses are now built when a new user signs up without overridung the Devise Registrations controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     if current_user.admin?
       "/admin"
     else 
       "/"
     end
+  end
+
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, addresses_attributes: [:address1, :address2, :city, :postcode]])
   end
 end
